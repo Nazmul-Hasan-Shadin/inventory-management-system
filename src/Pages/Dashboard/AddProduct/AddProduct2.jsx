a7a4d4c1f6e6db7e309fbdec0f8b94376f829445
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-12-28 22:01:58 +0600

    all product section has updated

--- src/Pages/Dashboard/AddProduct/AddProduct2.jsx
+++ src/Pages/Dashboard/AddProduct/AddProduct2.jsx
@@ -1,49 +1,69 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import useAxiosPublic from '../../../hooks/useaxiosPublic';
-import useaxiosSecure from '../../../hooks/useaxiosSecure';
-import ProductTable from '../ProductTable/ProductTable';
-import useAuth from '../../../hooks/useAuth';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { Link } from "react-router-dom";
+import useAxiosPublic from "../../../hooks/useaxiosPublic";
+import useaxiosSecure from "../../../hooks/useaxiosSecure";
+import ProductTable from "../ProductTable/ProductTable";
+import useAuth from "../../../hooks/useAuth";
+import { FaPlus } from "react-icons/fa";
 
 const AddProduct2 = () => {
-  const {user}=useAuth()
-    const axiosSecure= useaxiosSecure()
-    const { data:productCount,isLoading,refetch:refetchCount } = useQuery({
-        queryKey: ['pdCount'],
-        queryFn: async() => {
- 
-          const productCount= await axiosSecure.get(`/productCountByEmail/${user?.email}`)
- 
-          return productCount?.data?.count
-   
-         
-        }
-        
-      });
+  const { user } = useAuth();
+  const axiosSecure = useaxiosSecure();
+  const {
+    data: productCount,
+    isLoading,
+    refetch: refetchCount,
+  } = useQuery({
+    queryKey: ["pdCount"],
+    queryFn: async () => {
+      const productCount = await axiosSecure.get(
+        `/productCountByEmail/${user?.email}`
+      );
 
-      if (isLoading) {
-        return <p className='text-center'>Loading</p>
-      }
-   
-    return (
-        <div className='w-full'>
-            <input type="text"  readOnly defaultValue={` total  ${productCount} product has added`} className="input  input-bordered w-7/12" />
+      return productCount?.data?.count;
+    },
+  });
 
+  if (isLoading) {
+    return <p className="text-center">Loading</p>;
+  }
 
-            <Link to={'/dashboard/addproduct-final'}><button   className={`btn btn-outline btn-secondary ${''} `}>Add Product</button></Link>
+  return (
+    <div className="w-full">
+      {/* <input
+        type="text"
+        readOnly
+        defaultValue={` total  ${productCount} product has added`}
+        className="input  input-bordered w-7/12"
+      />
 
-            {/* =======================================show here all product============================================================ */}
+      <Link to={"/dashboard/addproduct-final"}>
+        <button className={`btn btn-outline btn-secondary ${""} `}>
+          Add Product
+        </button>
+      </Link> */}
 
-         <ProductTable refetchCount={refetchCount}></ProductTable>
-
-
-
-
-
-
+<div className="flex bg-[#FAFBFE] p-12  justify-between">
+        <div>
+          <h2 className="text-2xl">Total Product</h2>
+          <p>Manage Your Product</p>
         </div>
-    );
+
+        <Link to={"/dashboard/addproduct-final"}>
+        <button className="btn px-10 bg-[#FF9F43]">
+          {" "}
+          <FaPlus /> Add Product
+        </button>
+      </Link>
+        
+      </div>
+
+      {/* =======================================show here all product============================================================ */}
+
+      <ProductTable refetchCount={refetchCount}></ProductTable>
+    </div>
+  );
 };
 
 export default AddProduct2;
--- src/Pages/Dashboard/ProductTable/ProductTable.jsx
+++ src/Pages/Dashboard/ProductTable/ProductTable.jsx
@@ -1,4 +1,4 @@
-import { FaCross, FaEdit, FaTrashAlt } from "react-icons/fa";
+import { FaCross, FaEdit, FaFilePdf, FaFilter, FaList, FaPlus, FaPrint, FaTrashAlt } from "react-icons/fa";
 import useAllProducts from "../../../hooks/useAllProducts";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
@@ -7,6 +7,7 @@ import { Helmet } from "react-helmet-async";
 
 const ProductTable = ({ refetchCount }) => {
   const [products, refetch] = useAllProducts();
+  console.log(products);
   const axiosSecure = useaxiosSecure();
 
   //  ==========================handle Delete product=======================================
@@ -40,33 +41,55 @@ const ProductTable = ({ refetchCount }) => {
     });
   };
   return (
-    <div className="">
-  
-  <div className="w-[100%]">
+    <div>
+ 
+ <div className="flex bg-[#FFFFFF] p-12  justify-between">
+        <div className="flex gap-5 items-center">
+        <FaFilter  className="text-4xl"/>
+          <input type="text" placeholder="Type here" className="input input-bordered h-11 input-primary w-full  max-w-xs" />
+        </div>
+
+        <Link to={"/dashboard/addproduct-final"}>
+         <span className="text-2xl flex gap-3">
+          <FaPrint className="text-green-500"></FaPrint>
+          <FaFilePdf className="text-red-700"></FaFilePdf>
+          <FaList></FaList>
+         </span>
+      </Link>
+        
+      </div>
+
+      <div className="w-[100%]">
         <Helmet>
           <title> inven || control </title>
         </Helmet>
-        <table className="table ">
-          {/* head */}
-          <thead>
-            <tr>
+
+        <table className="table  ">
+          <thead className="bg-[#FAFBFE]">
+            <tr className="">
               <th>
                 <label>#</label>
               </th>
-              <th>Product Name and Image </th>
-              <th>Sale Count</th>
-              <th>Product Quantity</th>
+             
+              <th className="text-black">Product image </th>
+              <th className="text-black">Name</th>
+              <th className="text-black">Sale Count</th>
+              <th className="text-black">Product Quantity</th>
 
-              <th>update here</th>
-              <th>Delete</th>
+              <th className="text-black">selling price</th>
+              <th className="text-black">Buy price</th>
+              <th className="text-black">Location</th>
+              <th className="text-black">update here</th>
+              <th className="text-black">Delete</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="bg-[#FFFFFF]">
             {products?.product?.map((product, index) => (
-              <tr className="" key={product._id}>
+              <tr className="" key={product._id}  >
                 <th>
                   <label>{index + 1}</label>
                 </th>
+               
                 <td>
                   <div className="flex items-center gap-3">
                     <div className="avatar">
@@ -74,17 +97,35 @@ const ProductTable = ({ refetchCount }) => {
                         <img src={product?.image} />
                       </div>
                     </div>
-                    <div>
-                      <div className="text-sm opacity-50"> {product.name}</div>
-                    </div>
                   </div>
                 </td>
+                <td className="text-black">{product.name}</td>
                 <td>
                   <span className="badge badge-ghost badge-sm">
                     Sale Count: {product.saleCount}{" "}
                   </span>
                 </td>
+
                 <td> {product?.quantity} </td>
+
+                <td>
+                  <span className="badge badge-ghost badge-sm">
+                    $ {product?.sellingPrice}
+                  </span>
+                </td>
+
+                <td>
+                  <span className="badge badge-ghost badge-sm">
+                    $ {product?.buyPrice}
+                  </span>
+                </td>
+
+                <td>
+                  <span className="badge badge-ghost badge-sm">
+                    {product?.location}
+                  </span>
+                </td>
+
                 <td>
                   {" "}
                   <Link to={`/dashboard/update/${product._id}`}>
@@ -105,7 +146,7 @@ const ProductTable = ({ refetchCount }) => {
                     className="btn"
                   >
                     {" "}
-                    <FaTrashAlt></FaTrashAlt>{" "}
+                    <FaTrashAlt className="text-xl text-red-600"></FaTrashAlt>{" "}
                   </button>{" "}
                 </td>
               </tr>
@@ -113,8 +154,6 @@ const ProductTable = ({ refetchCount }) => {
           </tbody>
         </table>
       </div>
-
-
     </div>
   );
 };
