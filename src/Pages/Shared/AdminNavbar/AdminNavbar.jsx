c12ea752ae3ec8fc2fcc9f213231cbc3a581f7d4
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-01-02 23:57:23 +0600

    search bar has updated

--- src/Pages/Shared/AdminNavbar/AdminNavbar.jsx
+++ src/Pages/Shared/AdminNavbar/AdminNavbar.jsx
@@ -1,13 +1,18 @@
 
 import { GiEarthAfricaEurope } from "react-icons/gi";
 import { SiGmail } from "react-icons/si";
+import { IoIosSearch } from "react-icons/io";
 import { IoIosNotifications } from "react-icons/io";
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider/AutProvider";
 const AdminNavbar = () => {
+  const {user}= useContext(AuthContext)
     return (
         <div>
             <div className="navbar justify-between bg-base-100">
-            <div className="form-control   ">
-      <input type="text" placeholder="Search" className="input input-bordered w-60 bg-[#F6F6F7] h-10 md:w-auto" />
+            <div className="form-control  relative  ">
+      <input type="text" placeholder="Search" className="input input-bordered pl-12 w-60 bg-[#F6F6F7] h-10 md:w-auto" />
+      <IoIosSearch className="absolute start-3 text-xl top-1/3" />
     </div>
   {/* <div className="flex-1">
     <a className="btn btn-ghost text-xl">daisyUI</a>
@@ -19,16 +24,25 @@ const AdminNavbar = () => {
  <IoIosNotifications className="bg-[#F6F7F8] rounded-xl p-2" />
  </div>
     <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+    <div className="flex items-center gap-3">
+    <div tabIndex={0} role="button" className="btn flex btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <img alt={user?.photoURL} />
+         
         </div>
+    
+
       </div>
+      <div >
+      <h2>{user?.displayName}</h2>
+      <span className="text-sm ">super admin</span>
+      </div>
+    </div>
       <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
         <li>
           <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
+           {user?.displayName}
+     
           </a>
         </li>
         <li><a>Settings</a></li>
