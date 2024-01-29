cb0b65b6e76a6546e97f85315cb43b1a311fd0a8
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-01-29 11:02:14 +0600

    website introduction video has added

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
       )}
       <li>
         {" "}
-        <Link to="/watch-demo" target="_blank"> Watch Demo</Link>
+        <Link to="/watch-demo" target="_blank"> Watch Overview</Link>
       </li>
     </>
   );
--- src/Pages/watchVIde/WatchVideo.jsx
+++ src/Pages/watchVIde/WatchVideo.jsx
@@ -3,15 +3,7 @@ import React from 'react';
 const WatchVideo = () => {
   return (
     <div>
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/PohSjXM5AW0?si=Gyo3LT4PMXzdnOzr"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/xzBuHZ2zkCw?si=giJYakv3rNu0VSBC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 };
